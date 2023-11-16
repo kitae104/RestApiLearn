7d641fe372a27d3051b7d46666fc79a5a45a084f
Kitae's Github <aqua0405@gmail.com>
2023-11-16 18:11:54 +0900

    Update CustomizedResponseEntityExceptionHandler.java

--- src/main/java/kr/inhatc/spring/exceptions/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/kr/inhatc/spring/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -1,8 +1,11 @@
 package kr.inhatc.spring.exceptions;
 
 import kr.inhatc.spring.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -31,5 +34,26 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         errorDetails.setDetails(request.getDescription(false));
 
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * 메소드의 인자가 유효하지 않을 때 발생하는 예외를 처리하는 메소드
+     * @param ex 예외
+     * @param headers 응답 헤더
+     * @param status 응답 상태
+     * @param request 요청
+     * @return 에러 메시지를 갖는 객체
+     */
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                "전체 오류 개수 : " + ex.getErrorCount() + "개" +
+                " 첫번째 오류 : " + ex.getFieldError().getDefaultMessage(),
+                request.getDescription(false));
+
+
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }
