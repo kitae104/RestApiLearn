776596c468c2d7b5f0eaa6ce1d6e13bdc6c78226
Kitae's Github <aqua0405@gmail.com>
2023-11-16 18:12:04 +0900

    Update UserDto.java

--- src/main/java/kr/inhatc/spring/user/UserDto.java
+++ src/main/java/kr/inhatc/spring/user/UserDto.java
@@ -1,6 +1,8 @@
 package kr.inhatc.spring.user;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -12,7 +14,10 @@ import java.time.LocalDate;
 @Builder
 public class UserDto {
     private Long id;                // id
+    @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
     private String name;            // 이름
+
+    @Past(message = "생일은 과거 날짜만 가능합니다.")
     private LocalDate birthDate;    // 생일
 
     public void setId(Long id) {
