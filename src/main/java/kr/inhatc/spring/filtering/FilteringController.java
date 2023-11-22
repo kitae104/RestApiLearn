fd834050317ce1d198ab527b1a36869323937609
Kitae's Github <aqua0405@gmail.com>
2023-11-22 18:04:28 +0900

    Update FilteringController.java

--- src/main/java/kr/inhatc/spring/filtering/FilteringController.java
+++ src/main/java/kr/inhatc/spring/filtering/FilteringController.java
@@ -1,5 +1,9 @@
 package kr.inhatc.spring.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +14,29 @@ import java.util.List;
 public class FilteringController {
 
     @GetMapping("/filtering")
-    public SomeBean filtering(){
-        return new SomeBean("value1", "value2", "value3");
+    public MappingJacksonValue filtering(){
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("filed1", "filed3");
+        FilterProvider filters =  new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        mappingJacksonValue.setFilters(filters);
+
+        return mappingJacksonValue;
     }
 
     @GetMapping("/filtering-list")
-    public List<SomeBean> filteringList(){
-        return Arrays.asList(
+    public MappingJacksonValue filteringList(){
+        List<SomeBean> list = Arrays.asList(
                 new SomeBean("value1", "value2", "value3"),
                 new SomeBean("value4", "value5", "value6")
         );
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(list);
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("filed2", "filed3");
+        FilterProvider filters =  new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        mappingJacksonValue.setFilters(filters);
+
+        return mappingJacksonValue;
     }
 }
