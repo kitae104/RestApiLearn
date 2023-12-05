bed70a1c1293536f658c3615171d55b712485e05
Kitae's Github <aqua0405@gmail.com>
2023-12-05 11:25:35 +0900

    Update TodoController.java

--- src/main/java/kr/inhatc/spring/todo/TodoController.java
+++ src/main/java/kr/inhatc/spring/todo/TodoController.java
@@ -2,6 +2,8 @@ package kr.inhatc.spring.todo;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +21,18 @@ public class TodoController {
     public List<TodoDto> retriveTodos(@PathVariable("username") String username){
         log.info("==============> " + username);
         return todoService.findByUsername(username);
+    }
+
+    @GetMapping("/users/{username}/todos/{id}")
+    public TodoDto retriveTodo(@PathVariable("username") String username, @PathVariable("id") Long id){
+        log.info("==============> " + username + ", " + id);
+        return todoService.findById(id);
+    }
+
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable("username") String username, @PathVariable("id") Long id){
+        log.info("==============> " + username + ", " + id);
+        todoService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
